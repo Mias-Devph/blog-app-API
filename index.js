7974ca096e0851dcc5ea63b54d32d66373112a94
ceninajr <ceninajr@gmail.com>
2025-06-02 17:21:05 +0800

    update

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 
 const corsOptions = {
 	origin: [
-		'http://localhost:3000'
+		'http://localhost:3000',
+		'https://blog-app-client-livid.vercel.app/'
 		
 	],
 	credentials: true,
